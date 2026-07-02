58b22fe47b893206239c0785d56ba0db5c07c7a3
美游酱 <2303165851@qq.com>
2026-07-03 03:50:31 +0800

    Add files via upload

---
+++ 加载器.lua
@@ -0,0 +1,4 @@
+local repo = "https://raw.githubusercontent.com/MicLing-7877/Obsidian/main/"
+local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
+Library:Notify("The card-based system is loading", nil, 4590662766)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Devbenb/MiolKnbe/refs/heads/main/%E5%8D%A1%E5%AF%86%E7%B3%BB%E7%BB%9F.lua')))()
